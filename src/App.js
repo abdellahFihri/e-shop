a35e7f8961c483229a9976293ce7fc1d7b8316ac
abdellahfihri <abdellahfihri@gmail.com>
2019-11-30 13:13:22 +0100

    redirect page after sign in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch,Redirect } from "react-router-dom";
 import {connect} from 'react-redux'
 import {setCurrentUser}from './redux/user/user.action'
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header";
-import SignIn_SignUp from "./pages/signin-signup/signin-signup";
+import SignInSignUp from "./pages/signin-signup/signin-signup";
 import { auth, createUserProfileDocument } from "./firebase/firebase";
 import "./App.css";
 
@@ -48,14 +48,18 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignIn_SignUp} />
+          <Route exact path="/signin" render={()=>this.props.currentUser?(<Redirect to='./'/>):(<SignInSignUp/>)} /> {/* redirect to main page is user is signed in */}
         </Switch>
       </div>
     );
   }
 }
-
+ const mapStateToProps = ({user}) => {
+  return {
+    currentUser: user.currentUser
+  }
+}
  const mapDispatchToProps = (dispatch) => ({
  setCurrentUser:user=>dispatch(setCurrentUser(user))//dispatching the object n retrieving the user from it (currentUser)
 })
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/pages/signin-signup/signin-signup.jsx
+++ src/pages/signin-signup/signin-signup.jsx
@@ -3,10 +3,10 @@ import  SignIN from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up'
 import './signin-signup.style.scss';
 
-const SignIN_SignUp=()=>(
+const SignInSignUp=()=>(
     <div className="sign-in-sign-up">
         <SignIN/>
         <SignUp/>
     </div>
 )
-export default SignIN_SignUp
+export default SignInSignUp
